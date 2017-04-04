f2af6321c566d973fa4ed2780d730d8099e92e46
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2017-04-04 13:10:38 +0200

    zpis i odczyt cz. dalsza

--- src/pl/uni/lodz/wfis/java2017w/zajecia5/WriteToFile.java
+++ src/pl/uni/lodz/wfis/java2017w/zajecia5/WriteToFile.java
@@ -1,8 +1,7 @@
 package pl.uni.lodz.wfis.java2017w.zajecia5;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
 
 /**
  * Created by Krzysztof Podlaski on 04.04.2017.
@@ -12,9 +11,29 @@ public class WriteToFile {
     public static void main(String[] a) throws IOException{
         BufferedWriter writer = new BufferedWriter(new FileWriter("text2.txt"));
         String textToSave = "ALA MA kota a kot ma bzika";
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        System.in
+                )
+        );
         writer.write(textToSave);
         writer.newLine();
         writer.write(textToSave);
+        String line;
+        while ( (line = reader.readLine() )!=null ){
+            if (line.equals("STOP")) break;
+            //Zapis jak leci
+            writer.write(line);
+            writer.newLine();
+            //Zapis - każde słowo nowa linia
+            for( String s : line.split(" ")){
+                //Integer.parseInt(s);
+                writer.write(s);
+                writer.newLine();
+            }
+        }
+        reader.close();
+
         writer.close();
 
     }
